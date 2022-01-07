27e54e37de50e742baa7db6a19288c7328d4382b
greko <gyan2020cs093@abesit.edu.in>
2022-01-08 02:01:03 +0530

    added header section

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Welcome from '../components/Welcome'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
      
       <div className='gradient-bg-welcome '>
       <Navbar/>
+      <Header/>
       <Welcome/>
       </div>
     </div>
