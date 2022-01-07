c3dcc222b611b4032f2096aa1f7e9f55f4a0ff7c
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2022-01-08 00:20:29 +0530

    welcome

---
+++ client/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>welcome</h1>
+        </div>
+    )
+}
+
+export default Welcome
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
+import Welcome from '../components/Welcome'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <h1> THIS IS NFTasker (under development)</h1>
       <Navbar/>
+      <Welcome/>
     </div>
   )
 }
