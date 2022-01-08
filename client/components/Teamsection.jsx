ce458afe599c79155fae6faaf0fa518ff920e353
greko <gyan2020cs093@abesit.edu.in>
2022-01-08 10:23:28 +0530

    Update Teamsection.jsx

--- client/components/Teamsection.jsx
+++ client/components/Teamsection.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Teamsection = () => {
     return (
-       <h1>Teamsection</h1>
+<div>
+    <h1>Teamsection</h1>
+    
+    <div class="grid grid-cols-4 gap-4">
+        <div class="w h-64 bg-white">01</div>
+        <div>01</div>
+        <div>01</div>
+    </div>
+
+
+
+</div>
     )
 }
 
