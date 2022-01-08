e7d797be956d0708dae885983533e3fcb1cd4851
greko <gyan2020cs093@abesit.edu.in>
2022-01-08 10:38:52 +0530

    teamsection(test1)

--- client/components/Teamsection.jsx
+++ client/components/Teamsection.jsx
@@ -5,14 +5,11 @@ const Teamsection = () => {
 <div>
     <h1>Teamsection</h1>
     
-    <div class="grid grid-cols-4 gap-4">
-        <div class="w h-64 bg-white">01</div>
-        <div>01</div>
-        <div>01</div>
+    <div class="grid grid-cols-3 gap-6 place-content-center w-screen bg-slate-700" >
+        <div class="w h-64 bg-white space-x-2 space">01</div>
+        <div class="w h-64 bg-white space-x-2">01</div>
+        <div class="w h-64 bg-white space-x-2">01</div>
     </div>
-
-
-
 </div>
     )
 }
