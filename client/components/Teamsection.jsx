cee86f88532a79d36f7d279323476849b45f4c2e
KANISHAK CHAURASIA <kanishakchaurasia2@gmail.com>
2022-01-08 11:03:25 +0530

    glass

--- client/components/Teamsection.jsx
+++ client/components/Teamsection.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Teamsection = () => {
     return (
-<div>
+<div className='flex felx-col flex-1  w-full'>
     
-    <div className="grid grid-cols-3 text-white  p-2 gap-6 place-content-center items-center bg-glassmorphism-welcome" >
-        <div className="w h-64 white-glassmorphism space-x-2 space">01</div>
-        <div className="w h-64 white-glassmorphism space-x-2">01</div>
-        <div className="w h-64 white-glassmorphism space-x-2">01</div>
+    <div className="grid grid-cols-3 text-white gradient-bg-welcome w-full p-2 gap-6 place-content-center items-center bg-glassmorphism-welcome" >
+        <div className="w p-2 h-64 white-glassmorphism space-x-2 space">01</div>
+        <div className="w h-64 p-2 white-glassmorphism space-x-2">01</div>
+        <div className="w h-64 p-2 white-glassmorphism space-x-2">01</div>
     </div>
 </div>
     )
