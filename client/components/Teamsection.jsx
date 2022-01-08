c17e745049da6da168b6330f4588b01c6ac2ace3
greko <gyan2020cs093@abesit.edu.in>
2022-01-08 14:48:00 +0530

    Update Teamsection.jsx

--- client/components/Teamsection.jsx
+++ client/components/Teamsection.jsx
@@ -1,34 +1,41 @@
 import React from 'react'
 const Teamsection = () => {
     return (
-<div className='flex felx-col flex-1  w-full'>
+<div className='flex felx-col flex-auto  w-full flex-wrap'>
     
-    <div className="grid grid-cols-4 text-white gradient-bg-welcome w-full p-2 gap-6 place-content-center items-center bg-glassmorphism-welcome" >
-        <div className="w p-2 h-96 white-glassmorphism space-x-2 space">
-            <div className='h-48 w-64 m-auto rounded-sm'>
-            <img className='h-48 w-64 rounded-sm object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
+    <div className="flex flex-auto flex-row flex-wrap text-white gradient-bg-welcome p-2 gap-6 place-content-center items-center bg-glassmorphism-welcome" >
+        <div className="p-2 h-auto white-glassmorphism space-x-2 space basis-1/5 md:basis-1/3">
+            <div className='h-auto w-auto m-auto p-10'>
+                <img className='h-52 w-52 m-auto object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
                 <h1 className='text-center text-xl '>Individual</h1>
-            </div>
-        </div>
-        <div className="w h-96 p-2 white-glassmorphism space-x-2">
-        <div className='h-48 w-64 m-auto rounded-sm  '>
-                <img className='h-48 w-64 rounded-sm object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
-                <h1 className='text-center text-xl '>Team Collaboration</h1>
-            </div>
-        </div>
-        <div className="w h-96 p-2 white-glassmorphism space-x-2">
-        <div className='h-48 w-64 m-auto rounded-sm  '>
-                <img className='h-48  w-auto rounded-sm object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
-                <h1 className='text-center text-xl '>Join Organisation</h1>
+                <p className='text-base text-center mt-2 text-blue-400'>Individual person can create their own ToDo list and assign their Good and Bad habits and are awarded points for folloing good habits and points are decremented if doing bad habits</p>
             </div>
         </div>
 
-        <div className="w h-96 p-2 white-glassmorphism space-x-2">
-        <div className='h-48 w-64 m-auto rounded-sm  '>
-                <img className='h-48 w-64 rounded-sm object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
-                <h1 className='text-center text-xl '>Win NFTs</h1>
+        <div className="p-2 h-auto white-glassmorphism space-x-2 space basis-1/5 md:basis-1/3">
+            <div className='h-auto w-auto m-auto p-10'>
+                <img className='h-52 w-52 m-auto object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
+                <h1 className='text-center text-xl '>Team</h1>
+                <p className='text-base text-center mt-2 text-blue-400'>Join or Create your own Group and set you resolution and follow and Showoff your Perfect score withs NFTs</p>
             </div>
         </div>
+
+        <div className="p-2 h-auto white-glassmorphism space-x-2 space basis-1/5 md:basis-1/3">
+            <div className='h-auto w-auto m-auto p-10'>
+                <img className='h-52 w-52 m-auto object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
+                <h1 className='text-center text-xl '>Join Organisation</h1>
+                <p className='text-base text-center mt-2 text-blue-400'>Create tracks for you teambers and track then then verify then to set the score Board</p>
+            </div>
+        </div>
+
+        <div className="p-2 h-auto white-glassmorphism space-x-2 space basis-1/5 md:basis-1/3">
+            <div className='h-auto w-auto m-auto p-10'>
+                <img className='h-52 w-52 m-auto object-cover' src='https://www.notion.so/cdn-cgi/image/format=auto,width=664,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'></img>
+                <h1 className='text-center text-xl '>Win NFTs</h1>
+                <p className='text-base text-center mt-2 text-blue-400'>You will get NFTs as rewards,<br />Top 5 members will get rare NTFs Weekly</p>
+            </div>
+        </div>
+        
         
     </div>
 </div>
